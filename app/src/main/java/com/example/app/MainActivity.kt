e1ca272492608ee101b3a9f8e91752d3afe1131f
Momot-Mpa320-01 <maxi.nissan@mail.ru>
2022-01-13 19:40:14 +0700

    Добавил главную активити.

---
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.app
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+open class MainActivity : AppCompatActivity() {
+
+
+    companion object {
+        const val RC_SIGN_IN = 123
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
